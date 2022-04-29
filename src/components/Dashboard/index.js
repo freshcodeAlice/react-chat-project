9bddf84e940f41817ff8678bd575fed061bc961a
freshcodeAlice <freshjs.ofte@gmail.com>
2022-04-29 18:44:30 +0300

    Needs api function

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { UserContext } from '../../contexts'
 import Chat from '../Chat'
 import DialogList from '../DialogList'
 import MessageArea from '../MessageArea'
 import styles from './Dashboard.module.css'
 
 const Dashboard = () => {
+  const [user, setUser] = useState({
+    id: 1,
+    username: 'User',
+    imageSrc: '/userAvatar.png'
+  })
+
+  //useReducer
+
+  useEffect(() => {
+    // send the server request
+    // after that - put data in store (dispatch data to reducer)
+  })
+
   return (
-    <main className={styles.container}>
-      <DialogList />
-      <div className={styles['chat-wrapper']}>
-        <Chat />
-        <MessageArea />
-      </div>
-    </main>
+    <UserContext.Provider value={user}>
+      <main className={styles.container}>
+        <DialogList />
+        <div className={styles['chat-wrapper']}>
+          <Chat />
+          <MessageArea />
+        </div>
+      </main>
+    </UserContext.Provider>
   )
 }
 
